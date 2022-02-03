8ef76d87ae6c48d913ff3fecd577628621c3f568
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-02-03 20:35:33 +0200

    Condition for null values

--- script/js.js
+++ script/js.js
@@ -7,13 +7,22 @@ function calculateCompoundInterest(){
     var term = document.getElementById("termYears").value;
     var periodicPayments = document.getElementById("periodicPayments").value;
 
-    $.ajax({
-        url: docRoot + 'ajax/calculateAjax.php',
-        type: 'post',
-        data: {'action':'calculateAjax',"principleValue":principleValue,"interestRate":interestRate,"n":n,"term":term,"periodicPayments":periodicPayments},
-        success: function(response){
-            
-         document.getElementById("futureValue").innerHTML = "Future value of investment:<br>" + response;
-        }         
-    });
+    if(principleValue == "" || interestRate == "" || n == "" || term == "" || periodicPayments == ""){
+        alert("Please enter values!");
+    }else{
+        $.ajax({
+            url: docRoot + 'ajax/calculateAjax.php',
+            type: 'post',
+            data: {'action':'calculateAjax',"principleValue":principleValue,"interestRate":interestRate,"n":n,"term":term,"periodicPayments":periodicPayments},
+            success: function(response){
+                if(response == "false"){
+                    
+                }else{
+                    document.getElementById("futureValue").innerHTML = "Future value of investment:<br>" + response;
+                }
+             
+            }         
+        });
+    }
+   
 }
